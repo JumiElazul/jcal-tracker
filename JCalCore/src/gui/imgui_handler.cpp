4fa9b89ce5f48454f39fceca0b670a51d50f6121
Gregory Coomber <gr.coomber@gmail.com>
2024-01-28 14:11:08 -0800

    Working on datepicker

--- JCalCore/src/gui/imgui_handler.cpp
+++ JCalCore/src/gui/imgui_handler.cpp
@@ -10,6 +10,41 @@
 #include <imgui_impl_glfw.h>
 #include <imgui_impl_opengl3.h>
 
+const char* DatePicker::s_days[31] = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10",
+                                     "11", "12", "13", "14", "15", "16", "17", "18", "19", "20",
+                                     "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31" };
+
+const char* DatePicker::s_months[12] = { "January", "February", "March", "April", "May", "June",
+                                       "July", "August", "September", "October", "November", "December" };
+
+DatePicker::DatePicker(int year_range, int start_year)
+    : _year_range(year_range), _start_year(start_year)
+{
+    for (int i = 0; i < _year_range; ++i)
+    {
+        _years.push_back(std::to_string(_start_year + i));
+    }
+}
+
+void DatePicker::render()
+{
+    ImGui::Combo("Day", &_selected_day, s_days, IM_ARRAYSIZE(s_days));
+    ImGui::Combo("Month", &_selected_month, s_months, IM_ARRAYSIZE(s_months));
+
+    std::vector<const char*> year_c_str;
+    for (const auto& str : _years)
+    {
+        year_c_str.push_back(str.c_str());
+    }
+    ImGui::Combo("Year", &_selected_year, year_c_str.data(), year_c_str.size());
+
+    std::string selected_date = std::string(s_days[_selected_day + 1]) + " " +
+        std::string(s_months[_selected_month + 1]) + " " +
+        _years[_selected_year];
+
+    ImGui::Text("Selected Date: %s", selected_date.c_str());
+}
+
 ImGuiHandler::ImGuiHandler(JsonHandler& json_handler)
     : _json_handler(json_handler) { }
 
@@ -42,7 +77,7 @@ void ImGuiHandler::new_frame() const
     ImGui::NewFrame();
 }
 
-void ImGuiHandler::draw_window(const Vec2& framebuffer_size) const
+void ImGuiHandler::draw_window(const Vec2& framebuffer_size)
 {
     ImVec2 window_size{ framebuffer_size.x, framebuffer_size.y };
     ImVec2 window_pos{ 0, 0 };
@@ -60,6 +95,8 @@ void ImGuiHandler::draw_window(const Vec2& framebuffer_size) const
         static char meal_buffer[128];
         static char calorie_buffer[6];
 
+        std::map<std::string, std::vector<MealEntry>> meal_data = _json_handler.deserialize_json(_json_handler.get_default_json_filepath());
+
         ImGui::InputTextWithHint("##meal_description", "Meal Description", meal_buffer, IM_ARRAYSIZE(meal_buffer));
         ImGui::InputTextWithHint("##calories", "Calories", calorie_buffer, IM_ARRAYSIZE(calorie_buffer), ImGuiInputTextFlags_CallbackCharFilter, filter_characters);
 
@@ -67,8 +104,6 @@ void ImGuiHandler::draw_window(const Vec2& framebuffer_size) const
         {
             if (meal_buffer[0] != '\0' && calorie_buffer[0] != '\0')
             {
-                std::map<std::string, std::vector<MealEntry>> meal_data = _json_handler.deserialize_json(_json_handler.get_default_json_filepath());
-
                 MealEntry meal_entry;
                 meal_entry.meal_description = meal_buffer;
                 meal_entry.calories = std::stoi(calorie_buffer);
@@ -85,7 +120,12 @@ void ImGuiHandler::draw_window(const Vec2& framebuffer_size) const
             }
         }
 
+        ImGui::Separator();
+
+        _date_picker.render();
+
         ImGui::End();
+        //ImGui::ShowDemoWindow();
     }
 }
 
--- JCalCore/src/gui/imgui_handler.h
+++ JCalCore/src/gui/imgui_handler.h
@@ -1,9 +1,31 @@
 #pragma once
+#include <string>
+#include <vector>
 
 struct GLFWwindow;
 struct Vec2;
 class JsonHandler;
 class ImGuiInputTextCallbackData;
+
+class DatePicker
+{
+public:
+    DatePicker(int year_range = 10, int start_year = 2020);
+
+    void render();
+
+private:
+    int _selected_day = 1;
+    int _selected_month = 0;
+    int _selected_year = 0;
+
+    int _year_range;
+    int _start_year;
+    std::vector<std::string> _years;
+
+    static const char* s_days[31];
+    static const char* s_months[12];
+};
 
 class ImGuiHandler
 {
@@ -12,11 +34,12 @@ public:
     ~ImGuiHandler();
 
     void new_frame() const;
-    void draw_window(const Vec2& framebuffer_size) const;
+    void draw_window(const Vec2& framebuffer_size);
     void render() const;
 
 private:
     JsonHandler& _json_handler;
+    DatePicker _date_picker;
 
     void init(GLFWwindow* window, const Vec2& window_size);
 
